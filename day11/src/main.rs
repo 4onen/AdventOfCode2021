27284f5dea088097f7b3d8b639cc232f951c50ae
Matthew Dupree <mattjdupree@gmail.com>
2021-12-12 20:16:16 -0800

    Complete Day11 part 1.

---
+++ day11/src/main.rs
@@ -0,0 +1,63 @@
+fn part1(mut grid: Vec<Vec<u8>>) -> usize {
+    let mut total_flash_count: usize = 0;
+
+    for _ in 0..100 {
+        let mut check_stack = (0..grid.len())
+            .flat_map(|y| (0..grid[y].len()).map(move |x| (x, y)))
+            .collect::<Vec<(usize, usize)>>();
+
+        let mut flashes: Vec<(usize, usize)> = Vec::new();
+
+        while let Some((x, y)) = check_stack.pop() {
+            grid[y][x] += 1;
+            if grid[y][x] > 9 {
+                grid[y][x] = 0;
+                flashes.push((x, y));
+
+                if y > 0 {
+                    check_stack.push((x, y - 1));
+                    if x > 0 {
+                        check_stack.push((x - 1, y - 1));
+                    }
+                    if x < grid[y].len() - 1 {
+                        check_stack.push((x + 1, y - 1));
+                    }
+                }
+                if y < grid.len() - 1 {
+                    check_stack.push((x, y + 1));
+                    if x > 0 {
+                        check_stack.push((x - 1, y + 1));
+                    }
+                    if x < grid[y].len() - 1 {
+                        check_stack.push((x + 1, y + 1));
+                    }
+                }
+                if x > 0 {
+                    check_stack.push((x - 1, y));
+                }
+                if x < grid[y].len() - 1 {
+                    check_stack.push((x + 1, y));
+                }
+            }
+        }
+
+        total_flash_count += flashes.len();
+
+        flashes.into_iter().for_each(|(x, y)| {
+            grid[y][x] = 0;
+        });
+    }
+
+    total_flash_count
+}
+
+// 1665 correct
+
+fn main() {
+    let input = include_str!("../input.txt")
+        .lines()
+        .map(|l| l.chars().map(|c| c as u8 - '0' as u8).collect::<Vec<u8>>())
+        .collect::<Vec<_>>();
+
+    println!("Part 1: {}", part1(input.clone()));
+}

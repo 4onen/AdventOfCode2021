cf3d91ddfc40cea85a24f8a22a1ef2ad5bb47c05
Matthew Dupree <mattjdupree@gmail.com>
2021-12-13 09:14:57 -0800

    Complete Day11.

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -53,6 +53,67 @@ fn part1(mut grid: Vec<Vec<u8>>) -> usize {
 
 // 1665 correct
 
+fn part2(mut grid: Vec<Vec<u8>>) -> usize {
+    let mut step: usize = 0;
+
+    let mut check_stack: Vec<(usize, usize)>;
+    let mut flashes: Vec<(usize, usize)> = Vec::new();
+
+    loop {
+        step += 1;
+        check_stack = (0..grid.len())
+            .flat_map(|y| (0..grid[y].len()).map(move |x| (x, y)))
+            .collect::<Vec<(usize, usize)>>();
+
+        flashes.clear();
+
+        while let Some((x, y)) = check_stack.pop() {
+            grid[y][x] += 1;
+            if grid[y][x] > 9 {
+                grid[y][x] = 0;
+                flashes.push((x, y));
+
+                if y > 0 {
+                    check_stack.push((x, y - 1));
+                    if x > 0 {
+                        check_stack.push((x - 1, y - 1));
+                    }
+                    if x < grid[y].len() - 1 {
+                        check_stack.push((x + 1, y - 1));
+                    }
+                }
+                if y < grid.len() - 1 {
+                    check_stack.push((x, y + 1));
+                    if x > 0 {
+                        check_stack.push((x - 1, y + 1));
+                    }
+                    if x < grid[y].len() - 1 {
+                        check_stack.push((x + 1, y + 1));
+                    }
+                }
+                if x > 0 {
+                    check_stack.push((x - 1, y));
+                }
+                if x < grid[y].len() - 1 {
+                    check_stack.push((x + 1, y));
+                }
+            }
+        }
+
+        if flashes.len() == grid.len() * grid[0].len() {
+            break;
+        }
+
+        flashes.iter().for_each(|&(x, y)| {
+            grid[y][x] = 0;
+        });
+    }
+
+    step
+}
+
+// 235 correct
+
 fn main() {
     let input = include_str!("../input.txt")
         .lines()
@@ -60,4 +121,5 @@ fn main() {
         .collect::<Vec<_>>();
 
     println!("Part 1: {}", part1(input.clone()));
+    println!("Part 2: {}", part2(input));
 }
